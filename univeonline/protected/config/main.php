b2e0230e8fdfc1ca91fe2ed18b5144f0fe1d7229
marcospha <marcospha@ceed4f58-e1d7-6877-69ef-712b69003146>
2013-08-29 04:21:42 +0000

    Agregado archivo .bat para compilar less

--- univeonline/protected/config/main.php
+++ univeonline/protected/config/main.php
@@ -11,8 +11,9 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'UniveOnline',
+	'language' => 'es',
+	'sourceLanguage' => 'en',
 	'theme'=>'bootstrap',
-	//'theme'=>'bootstrap',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
